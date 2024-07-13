5604eda61d66eb51aa74bfdbebf908285629ef9e
Nilesh410 <shirudenileshs@gmail.com>
2024-07-13 23:28:29 +0530

    add day 1 activity 4

---
+++ Day_1/Activity_4.js
@@ -0,0 +1,12 @@
+//Activity 4: Reassigning Variables
+
+//Task 5:- Declare a variable using let, assign it an initial value, reassign a new value, 
+//and log both values to the console
+
+let data=12345
+console.log("data=",data)
+{
+    data="Data"
+    console.log("data=",data)
+}
+console.log("data=",data)
