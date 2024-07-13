1a6e13c9e25f1bc9d4a97ef0641e9072b54448ac
Nilesh410 <shirudenileshs@gmail.com>
2024-07-13 23:27:36 +0530

    add day 1 activity 3

---
+++ Day_1/Activity_3.js
@@ -0,0 +1,27 @@
+//Activity:-3 Data Types
+
+//Task 4:- Create variables of different data types(number,string,boolean,object,array) 
+//and log each variable type using typeof operator
+
+let num=23
+console.log("type of num=",typeof(num))
+
+var str="Welcome to JAVA Script Challenge"
+console.log("type of message=",typeof(str))
+
+const power_active=true
+console.log("type of power active=",typeof(power_active))
+
+let emp_info={
+     name:"Abc Xyz",
+     company_name:"PQRS",
+     profile:"Software Engineer",
+     experience_year:10
+}
+console.log("type of emp_info=",typeof(emp_info))
+
+let arr=[10,"MAH",34.2,true]
+console.log("type of arr=",typeof(arr))
+let arr1=new Array("MAH","GUJ","UP","MP")
+console.log("type of arr1=",typeof(arr1))
+
