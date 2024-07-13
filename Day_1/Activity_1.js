22faf636221ecad1a29c4411edad98eea8f73cb9
Nilesh410 <shirudenileshs@gmail.com>
2024-07-13 23:23:22 +0530

    add day 1 activity 1

---
+++ Day_1/Activity_1.js
@@ -0,0 +1,9 @@
+//Activity 1:-> Variable Declaration 
+
+// Task 1:- Declare the variable using var, assign it a number, and log the value to the console.
+var num=23.45
+console.log("number=",num)
+
+// Task 2:- Declare the variable using let, assign it a stirng and log the value to the console.
+let str="Accept the coding challenge of JS"
+console.log("string data=",str)
