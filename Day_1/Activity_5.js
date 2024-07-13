690a8160329c6d848b1b9166534423925809d745
Nilesh410 <shirudenileshs@gmail.com>
2024-07-13 23:29:25 +0530

    add day 1 activity 5

---
+++ Day_1/Activity_5.js
@@ -0,0 +1,8 @@
+//Understading const
+//Task 6:-Try reassigning a variable declared with const and observe the error
+
+const indian_capital="Delhi"
+console.log("indian_capital=",indian_capital)
+indian_capital="Mumbai"
+console.log("indian_capital=",indian_capital)
+//Error:- TypeError: Assignment to constant variable.
