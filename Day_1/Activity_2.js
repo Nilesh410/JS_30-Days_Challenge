163b4c50a3f3831a8b3f0678a03ff382ab104480
Nilesh410 <shirudenileshs@gmail.com>
2024-07-13 23:26:09 +0530

    add day 1 activity 2

---
+++ Day_1/Activity_2.js
@@ -0,0 +1,6 @@
+//Activity 2:- Declaration of const variable 
+
+//Task 3:- Declare a variable using const, assign it boolean value, and log the value to the console
+
+const boolean_data=true;
+console.log("boolean_data=",boolean_data);
