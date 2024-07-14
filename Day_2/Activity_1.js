30af44684e045512f577d2857784cc64f9b2d2d7
Nilesh410 <shirudenileshs@gmail.com>
2024-07-15 00:04:54 +0530

    add result

--- Day_2/Activity_1.js
+++ Day_2/Activity_1.js
@@ -1,1 +1,14 @@
-console.log(a);
+let num1=10;
+let num2=23;
+
+//Addition
+let add=num1+num2;
+console.log(add);
+
+let sub=num1-num2;
+console.log(sub)
+
+let mul=num1*num2;
+console.log(mul)
+
+let div=num1/num2;
