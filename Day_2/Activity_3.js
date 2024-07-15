dea8e753a9358c65e6ed7e930499739b23c77aa1
Nilesh410 <shirudenileshs@gmail.com>
2024-07-15 19:43:17 +0530

    add day 2 activity 2j

---
+++ Day_2/Activity_3.js
@@ -0,0 +1,16 @@
+//Activity 3- Comparison Operator
+
+//Task 8
+let num1=23
+let num2=23.56
+
+console.log(`num1 ${num1} > num2 ${num2} => ${num1>num2}`)
+console.log(`num1 ${num1} < num2 ${num2} => ${num1<num2}`)
+
+//Task 9 Greather than and less than Operator 
+console.log(`num1 ${num1} >= num2 ${num2} => ${num1>=num2}`)
+console.log(`num1 ${num1} <= num2 ${num2} => ${num1<=num2}`)
+
+//Task 10
+console.log(`num1 ${num1} == num2 ${num2} => ${num1==num2}`)
+console.log(`num1 ${num1} === num2 ${num2} => ${num1===num2}`)
