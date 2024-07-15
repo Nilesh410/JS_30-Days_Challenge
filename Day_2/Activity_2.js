40f1cd91436736922cc1725b46d3df33773fab31
Nilesh410 <shirudenileshs@gmail.com>
2024-07-15 19:27:47 +0530

    add day 2 activity_2

---
+++ Day_2/Activity_2.js
@@ -0,0 +1,10 @@
+// Activity 2:- Assignment Operator
+// Task 6 -> Use the += to add a number to variable and log the result on console
+
+let num1=56;
+num1+=4  //short hand plus assignment operator
+console.log(num1)
+
+// Task 7 -> Use the -= to substract a number to variable and log the result on console
+num1-=7   //short hand minus assignment operator
+console.log(num1) 
