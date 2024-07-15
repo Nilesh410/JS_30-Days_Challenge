e0323749467e154363586831ce4f8587d3cb96cc
Nilesh410 <shirudenileshs@gmail.com>
2024-07-15 19:19:37 +0530

    add code of activity_2 and theri all task

--- Day_2/Activity_1.js
+++ Day_2/Activity_1.js
@@ -1,14 +1,22 @@
 let num1=10;
 let num2=23;
 
-//Addition
+//Task 1:- Addition
 let add=num1+num2;
-console.log(add);
+console.log(`Addition of ${num1} and ${num2}= ${add}`);
 
+//Task 2:- Substraction 
 let sub=num1-num2;
-console.log(sub)
+console.log(`Substraction of ${num1} and ${num2}= ${sub}`);
 
+//Task 3:- Multiplication
 let mul=num1*num2;
-console.log(mul)
+console.log(`Multiplication of ${num1} and ${num2}= ${mul}`);
 
+//Task 4:- Division 
 let div=num1/num2;
+console.log(`Division of ${num1} and ${num2}= ${div}`);
+
+//Task 5:- Division
+let reminder=num2%num1
+console.log(`Reminder when division of ${num2} and ${num1}= ${reminder}`);
