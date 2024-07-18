6ee683515b4236842896d50fbcf35d640f0633c8
Nilesh410 <shirudenileshs@gmail.com>
2024-07-18 14:00:44 +0530

    add solution of day 3 activity 3

---
+++ Day_3/Activity_3.js
@@ -0,0 +1,23 @@
+//Activity 3:- Switch case
+//Task 4:-Write a program that uses switch case to determine the day of the week based on number (1-7) and log the day name to console.
+
+let day_number=5
+
+switch(day_number)
+{
+    case 1:console.log("Day of the week is Monday")
+           break
+    case 2:console.log("Day of the week is Tuesday")
+           break
+    case 3:console.log("Day of the week is Wednesday")
+           break
+    case 4:console.log("Day of the week is Thursday")
+           break
+    case 5:console.log("Day of the week is Friday")
+           break
+    case 6:console.log("Day of the week is Saturday")
+           break
+    case 7:console.log("Day of the week is Sunday")
+           break
+    default:console.log("Invalid Inpput")
+}
