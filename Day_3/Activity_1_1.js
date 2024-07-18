1ccd838fca41ac99b063f31b7e2d760a92b39c3e
Nilesh410 <shirudenileshs@gmail.com>
2024-07-18 13:39:21 +0530

    add solution of Day 3 activity 1

---
+++ Day_3/Activity_1_1.js
@@ -0,0 +1,21 @@
+// Day 3:- Control Structures 
+//Activity 1: If else Statements
+
+//Task 1:- Write a program to check if a number is positive, negative and zero and log the result to the console.
+ 
+let given_number=10
+given_number=0
+
+if (given_number===0)
+   console.log("number is zero")
+else
+{
+    if(given_number>0)
+        console.log("number is positive")
+    else
+        console.log("number is negative")
+}
+
+  
+
+//Task 2:- Write a program to check if a person is elgile to vote (age>=18) and log the result to the console
---
+++ Day_3/Activity_1_2.js
@@ -0,0 +1,8 @@
+//Task 2:- Write a program to check if a person is elgile to vote (age>=18) and log the result to the console
+
+let age_of_person=23
+
+if(age_of_person>18)
+   console.log("Person is eligble for voting");
+else
+   console.log("Person is not applicable for voting");
