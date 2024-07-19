28d19befd02c3aaf5e17cad086be821aa6f7af6c
Nilesh410 <shirudenileshs@gmail.com>
2024-07-19 15:04:48 +0530

    add sol of activity 1 of day 4

---
+++ Day_4/Activity_1.js
@@ -0,0 +1,16 @@
+//Activity_1 :- For Loop
+//Task 1:- Write a program to print numbers from 1 to 10 using a loop
+
+for (let i=1;i<=10;i++)
+{
+    console.log(i)
+}
+
+//Task 2:- Write a program to print the multiplication table of 5 using for loop
+
+let num=5
+console.log(`Multiplication Table of ${num}=`)
+for(let i=1;i<=10;i++)
+{
+    console.log(`${num}*${i}=${num*i}`)
+}
