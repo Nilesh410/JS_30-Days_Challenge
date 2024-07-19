92563c6651247a8d6ae8b5c834ef1979de6b178b
Nilesh410 <shirudenileshs@gmail.com>
2024-07-19 15:50:02 +0530

    add sol of activity_2 day 4

---
+++ Day_4/Activity_2.js
@@ -0,0 +1,12 @@
+//Activity-2 While loop
+//Task 3:- Write a progrma to calculate the sum of numbers from 1 to 10 using a while loop
+
+let num=10;
+let cond_var=1;
+let sum=0;
+while(cond_var<=num)
+{
+     sum+=cond_var;
+     cond_var++;
+}
+console.log("sum of 1 to 10 number="+sum)
