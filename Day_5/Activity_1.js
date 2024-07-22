6d485923f958238617783a48033447bc16be6d9f
Nilesh410 <shirudenileshs@gmail.com>
2024-07-22 16:52:38 +0530

    define function using function decleration

---
+++ Day_5/Activity_1.js
@@ -0,0 +1,12 @@
+//Day 5:- Functions
+//Activity_1:- Function Decleration 
+//Task 1:- Write a function to check if a number is even or odd and log the result on console.
+
+function even_odd(num)
+{
+    if(num%2===0)
+        console.log(`Given number ${num} is an even number`)
+    else
+        console.log(`given number ${num} is an odd number`)
+}
+even_odd(467)
