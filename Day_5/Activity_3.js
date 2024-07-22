c9bbf0ca547eba82357fee844cdd54358d590834
Nilesh410 <shirudenileshs@gmail.com>
2024-07-22 17:22:01 +0530

    design the arrow function for add

---
+++ Day_5/Activity_3.js
@@ -0,0 +1,9 @@
+//Activity 3:- Arrow Function 
+//Task 5:- Write a arrow function to calculate the sum of two numbers and return the result
+let no1=34
+let no2=23
+
+let add =(num1,num2)=> num1+num2;
+
+console.log(`${no1} + ${no2}=${add(no1,no2)}`)
+
