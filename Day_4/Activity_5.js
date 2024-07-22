c9eb115ac8cebe6bc79479ed02b1e3da22a9ac0f
Nilesh410 <shirudenileshs@gmail.com>
2024-07-22 15:14:38 +0530

    study use of break and continue

---
+++ Day_4/Activity_5.js
@@ -0,0 +1,19 @@
+//Activity_5 :- Loop Control Statements
+//Task 8:- Write a program to print numbers from 1 to 10, but the skip the number 5 using the continue statement
+
+for (let i=1;i<=10;i++)
+{
+    if(i===5)
+        continue
+    else
+        console.log(i)
+}
+
+//Task 9:- Write a program to print numbers from 1 to 10,but stop the loop when the number is 7 using break statement
+for(let i=1;i<=10;i++)
+{
+    if(i===7)
+        break
+    else
+        console.log(i)
+}
