fd74fdc27db3d5909fa81835417897525a07f0e2
Nilesh410 <shirudenileshs@gmail.com>
2024-07-22 17:07:15 +0530

    define func expression to find max between two

---
+++ Day_5/Activity_2.js
@@ -0,0 +1,12 @@
+//Activity 2:- Function Expression
+//Task 3:- Write a function expression to find the maximum of two numbers and log the result on the console
+let no1=23
+let no2=34
+
+let max_num=function (num1,num2)
+{
+    console.log(`maximum number is ${num1>num2 ? num1 : num2}`)
+}
+
+max_num(no1,no2)
+
