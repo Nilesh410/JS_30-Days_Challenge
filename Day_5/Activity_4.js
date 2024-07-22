7fea8ad34ff7f3acac208edcf4e51e82168e1913
Nilesh410 <shirudenileshs@gmail.com>
2024-07-22 17:49:27 +0530

    function with default value

--- Day_5/Activity_4.js
+++ Day_5/Activity_4.js
@@ -1,4 +1,4 @@
-//Activity 5:- Function Parameters and Default values
+//Activity 4:- Function Parameters and Default values
 //Task 7:- Write a function that takes two parameters and returns
 //their product. Provide a default value for the second parameter
 let no1=6
@@ -7,3 +7,11 @@ let product=(num1,num2=5)=>num1*num2
 
 console.log(`${no1}*${no2}=${product(no1,no2)}`)
 
+//Task 8: Write a function that takes a person's name and age and returns 
+//a greeting message. Provide a default value for the age
+let person_name="Nilesh Shirude"
+let person_age
+let message=(name,age=34)=>{
+    return "Hello " +name+ " your age is "+age
+}
+console.log(message(person_name,person_age))
