a2cf45a98b9874579e67ace5e0fef74fd68956f7
Nilesh410 <shirudenileshs@gmail.com>
2024-07-22 17:42:06 +0530

    function with default parameter

---
+++ Day_5/Activity_4.js
@@ -0,0 +1,9 @@
+//Activity 5:- Function Parameters and Default values
+//Task 7:- Write a function that takes two parameters and returns
+//their product. Provide a default value for the second parameter
+let no1=6
+let no2=2
+let product=(num1,num2=5)=>num1*num2
+
+console.log(`${no1}*${no2}=${product(no1,no2)}`)
+
