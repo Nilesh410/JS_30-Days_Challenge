f532cf51d6ba9a698db8cd7680f5cf0073ab5f73
Nilesh410 <shirudenileshs@gmail.com>
2024-07-25 23:54:22 +0530

    add activity_1 day 6 solution

---
+++ Day_6/Activity_1.js
@@ -0,0 +1,11 @@
+//Day_6
+//Activity_1:- Create an array of numbers from 1 to 5 and log the array of console
+
+let arr=new Array(1,2,3,4,5)
+console.log(arr) // display the array elements
+//Task :- Access first and last element of array
+//accss first element
+//console.log("First_ele=",arr.find)
+console.log("First ele=",arr[0])
+console.log("Last Ele=",arr[arr.length-1])
+//Acc
