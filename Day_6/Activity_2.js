e16005f109dc1fc838eb61a03cc0d835f2b90587
Nilesh410 <shirudenileshs@gmail.com>
2024-08-03 23:58:31 +0530

    add the element at the end in array ushing push

---
+++ Day_6/Activity_2.js
@@ -0,0 +1,7 @@
+//Activity 2 Array Basic Methods
+//Task 3:- Use the push method to add a new number to end of the array and log the updated array
+
+let arr=new Array(1,2,3,4,5)
+console.log(arr)
+arr.push("NSS")
+console.log(arr)
