1f162a66d875f95d8dd0137bbc7ff4c7ae3e1e70
Nilesh410 <shirudenileshs@gmail.com>
2024-08-04 00:04:25 +0530

    add use of pop method in array

--- Day_6/Activity_2.js
+++ Day_6/Activity_2.js
@@ -2,6 +2,14 @@
 //Task 3:- Use the push method to add a new number to end of the array and log the updated array
 
 let arr=new Array(1,2,3,4,5)
+console.log("Before push the array elements=")
 console.log(arr)
 arr.push("NSS")
+console.log("After push array elements are=")
+console.log(arr)
+
+
+//Task 4: Use the pop method to remove last element from the array and log the updated array
+let remove_ele=arr.pop()
+console.log("remove_ele=",remove_ele)
 console.log(arr)
