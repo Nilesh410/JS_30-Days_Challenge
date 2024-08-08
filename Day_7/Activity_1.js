ba22bd0fee47dcfe4ef51159f94178b22d8e74fb
Nilesh410 <shirudenileshs@gmail.com>
2024-08-08 19:50:56 +0530

    create an object and access its member

---
+++ Day_7/Activity_1.js
@@ -0,0 +1,12 @@
+//Day 7:- Objects
+//Activity_1:- Object Creation and access
+//Task 1:- Creae an object representing a book with 
+//perperties like title,author, and year and log the object to console
+let book_details={
+    title:"Mrutanjay",
+    author:"Shivaji Sawant",
+    year:2024
+}
+console.log(book_details)
+//Task 2:- Access and log the title and author properties from book object
+console.log(`book titlle: ${book_details.title} and author is ${book_details.author}`)
