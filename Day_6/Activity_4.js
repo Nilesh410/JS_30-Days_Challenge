3201a6c2c8005fcb6070c12b977bac1c6ded751a
Nilesh410 <shirudenileshs@gmail.com>
2024-08-08 17:03:57 +0530

    iterate array elements using for each

---
+++ Day_6/Activity_4.js
@@ -0,0 +1,6 @@
+//Activity_4:- Array Iteration
+//Task 10:Use a for loop to iterate over the array and log each element to the console
+let arr=new Array("Nilesh","PICT",4,"October",1989)
+arr.forEach(element => {
+    console.log(element);
+});
