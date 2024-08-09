f78ceb2cc80e09aa742a9da0b57c517cc8f6de63
Nilesh410 <shirudenileshs@gmail.com>
2024-08-09 14:22:42 +0530

    nesting of oject

--- Day_7/Activity_3.js
+++ Day_7/Activity_3.js
@@ -1,7 +1,7 @@
 //Activity 3:- Nested Object
 //Task 5:- Create a nested object resepresenting a library with properties like name and books 
 //(an array of book objects)and log the library object to the console.
-
+//Task 6:- Access and log the name of the library and the titles of all the books in the library
 let library={
     name:"The Great Indian Library",
     books:{
